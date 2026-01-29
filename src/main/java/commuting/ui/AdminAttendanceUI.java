1b715fa9c49dbaaf058d85a5101157dd31de4ab2
rainydg53-star <rainydg53@gmail.com>
2026-01-29 16:22:11 +0900

    브랜치 생성 확인

--- src/main/java/commuting/ui/AdminAttendanceUI.java
+++ src/main/java/commuting/ui/AdminAttendanceUI.java
@@ -1,4 +1,12 @@
 package commuting.ui;
 
 public class AdminAttendanceUI {
+    static void main() {
+        int a,b;
+        b  =10;
+        a = 100;
+        int c= a+b;
+        System.out.println(a+b+c);
+
+    }
 }
